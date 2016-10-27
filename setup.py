1c5657c7b7f3bd5f41c4ea88ff4e21fcd77bb005
PyB1l <vpapavasil@gmail.com>
2016-10-27 17:19:36 +0300

    Added travis ci config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lunatic',
+    version='1.0',
+    packages=['tests', 'lunatic'],
+    url='https://github.com/agile4you/lunatic',
+    license='GLPv3',
+    author='pav',
+    author_email='vpapavasil@gmail.com',
+    description='PostgreSQL Python booster utilities'
+)
