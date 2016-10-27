8fbaf1cc40eb1692d1ff91e9e455ca7612c17225
PyB1l <vpapavasil@gmail.com>
2016-10-27 17:44:57 +0300

    Auto setup version

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
+from lunatic import  __version__
+
+version = '.'.join(__version__)
 
 setup(
     name='lunatic',
-    version='1.0',
+    version=version,
     packages=['tests', 'lunatic'],
     url='https://github.com/agile4you/lunatic',
     license='GLPv3',
