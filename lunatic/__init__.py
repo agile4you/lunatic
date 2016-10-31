c88292152134fdf14dbb7e698ec3527093369b6f
PyB1l <vpapavasil@gmail.com>
2016-10-31 14:31:02 +0200

    Added version 1.1.1

--- lunatic/__init__.py
+++ lunatic/__init__.py
@@ -23,7 +23,7 @@ Postgresql python booster package!
 __all__ = ('DBEngine', 'DBEngineError', 'DBRouter', 'DBRouterError', 'QueryManager', 'QueryManagerError',
            'pubsub', 'patch')
 
-__version__ = '1.1.2'
+__version__ = '1.1.1'
 
 from lunatic.engine import DBEngine, DBRouter
 from lunatic.query import QueryManager
